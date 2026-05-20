e0bf40ce24fcd4607758559af6ed2011f4477c34
AgarwalGouraav <agarwal2005gourav@gmail.com>
2026-05-21 00:40:45 +0530

    Add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -141,7 +141,7 @@ const loginUser = asyncHandler(async (req,res) => {
         new ApiResponse(
             200,
             {
-                user: loggedinUser , accessToken , refreshToken
+                user: loggedInUser , accessToken , refreshToken
             },
             "User logged In Successfully"
         )
